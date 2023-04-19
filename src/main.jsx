c06af58061d03f1be9315f80f563360c67eaf11d
Nyakamba <omwegaenock@gmail.com>
2023-04-19 20:28:37 +0300

    main,jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-import NavBar from "./App";
+import App from "./App";
 
-ReactDOM.render(<NavBar />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
