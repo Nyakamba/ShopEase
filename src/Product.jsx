3a7196c7066d733bc75210c47f70f190082a65ae
Nyakamba <omwegaenock@gmail.com>
2023-04-21 20:11:53 +0300

    product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Product extends Component {
   state = {
     id: this.props.id,
-    productName: this.props.productname,
+    productName: this.props.productName,
     price: this.props.price,
   };
   render() {
@@ -12,9 +12,9 @@ export default class Product extends Component {
       <div className="col-lg-6">
         <div className="card m-2">
           <div className="card-body">
-            <div className="text-muted">#{this.props.id}</div>
-            <h5 className="pt-5 border-top">{this.props.productName}</h5>
-            <div>${this.props.price}</div>
+            <div className="text-muted">#{this.state.id}</div>
+            <h5 className="pt-5 border-top">{this.state.productName}</h5>
+            <div>${this.state.price}</div>
           </div>
         </div>
       </div>
