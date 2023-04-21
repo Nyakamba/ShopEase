43cef3086455a91e7e49566d5c529fece500c10a
Nyakamba <omwegaenock@gmail.com>
2023-04-21 15:27:21 +0300

    shoppingCart

---
+++ src/ShoppingCart.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import CustomersList from "./CustomersList";
+
+export default class ShoppingCart extends Component {
+  render() {
+    return <div>Shopping Cart</div>;
+  }
+}
