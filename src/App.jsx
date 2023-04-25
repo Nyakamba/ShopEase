eaedc0d0b083a1578353a8bbaa1db01ba84af110
Nyakamba <omwegaenock@gmail.com>
2023-04-25 13:02:29 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import ShoppingCart from "./ShoppingCart";
-//import Login from "./Login";
+import Login from "./Login";
+import CustomersList from "./CustomersList";
+import Dashboard from "./Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
 
 export default class App extends Component {
   render() {
     return (
-      <>
+      <BrowserRouter>
         <NavBar />
-        <ShoppingCart />
-      </>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/customers" exact element={<CustomersList />} />
+          <Route path="/cart" exact element={<ShoppingCart />} />
+          <Route path="*" element={<NoMatchPage />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
